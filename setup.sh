0b17c43b180e9561230c7e989d6288fe2935fae8
Matteo Schmider <matteo.schmider@student.kit.edu>
2022-12-31 23:08:58 +0100

    include system setup

--- setup.sh
+++ setup.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 
-# fail if an error occurs on any of the commands
-set -e
-
-# check if run as sudo
-if [ "$EUID" -ne 0 ]
-  then echo "Please run with sudo!"
-  exit
-fi
-
-# remove user specific keybind config
-rm -rf /home/jukebox/.config/openbox
-# replace system wide keybind config with our modified one
-cp -f lxde-pi-rc.xml /etc/xdg/openbox/lxde-pi-rc.xml
-
-# install dependency for keybinder script
-pip install -U click yaml requests
-# install the keybinder script as command line tool
-cp -f keybinder /usr/local/bin/keybinder
-# install the jukeboxcli script as command line tool
-cp -f jukeboxcli /usr/local/bin/jukeboxcli
-
-echo "Setup was successful!"
+. setup_system.sh
+. setup_keybinds.sh
---
+++ setup_system.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# fail if an error occurs on any of the commands
+set -e
+
+# check if run as sudo
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo!"
+  exit
+fi
+
+###########################################################
+# SETUP DEBUG STUFF
+###########################################################
+# set to 1 if on RPI, 0 on desktop
+RPI=0
+
+LMS="http://downloads.slimdevices.com/LogitechMediaServer_v8.3.0/logitechmediaserver_8.3.0_arm.deb"
+SQUEEZE="https://sourceforge.net/projects/lmsclients/files/squeezelite/linux/squeezelite-1.9.9.1392-aarch64.tar.gz/download"
+
+if [ "$RPI" -eq "0" ];
+then
+    LMS="http://downloads.slimdevices.com/LogitechMediaServer_v8.3.0/logitechmediaserver_8.3.0_amd64.deb"
+    SQUEEZE="https://sourceforge.net/projects/lmsclients/files/squeezelite/linux/squeezelite-1.9.9.1414-x86_64.tar.gz/download"
+fi
+###########################################################
+# INSTALL LMS
+###########################################################
+# get new packages
+apt-get update
+# setup user for autostart stuff
+useradd -m jukeboxuser || true
+
+# get the LMS debian package
+wget ${LMS} -O lms.deb
+# install it
+dpkg -i lms.deb
+rm lms.deb
+apt --fix-broken install
+systemctl daemon-reload
+systemctl enable logitechmediaserver.service
+systemctl start logitechmediaserver.service
+###########################################################
+# INSTALL SQUEEZELITE
+###########################################################
+# get the squeezelite archive
+wget ${SQUEEZE}
+# extract the files from the archive and go into the folder
+tar -xzf download
+# move the squeezelite application to somewhere
+mv squeezelite /usr/bin/squeezelite -f
+# clean up downloaded files
+rm LICENSE.txt
+rm download
+###########################################################
+# INSTALL AUTOSTART
+###########################################################
+if [ "$RPI" -eq "0" ];
+then
+    exit 0
+fi
+# copy modified system files for autostart
+cp desktop.conf /etc/xdg/lxsession/LXDE-pi/desktop.conf -f
+cp autostart /etc/xdg/openbox/autostart -f
